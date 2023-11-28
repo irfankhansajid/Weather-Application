b8b699356118da6614bb3ec7fc4251fbecd0df6b
irfankhansajid <irfankhansajid@gmail.com>
2023-11-29 03:40:18 +0600

    Weather Application in one commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Weather from './Component/Weather/Weather.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <Weather />
+      </div>
+    </>
+  )
+}
+
+export default App
